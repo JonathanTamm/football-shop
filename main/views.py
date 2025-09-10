4a9dc779d0f9ad63f72ae42a49fd5adad1ff62ca
JonathanTamm <jonathan.immanuel41@ui.ac.id>
2025-09-10 09:34:12 +0700

    selesai

--- main/views.py
+++ main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'app_name': 'Football Shop', 
+        'app_name': 'Football Shop',
+        'user_name': 'Jonathan Immanuel Tampubolon',
+        'user_npm': '2406395695',
+        'user_class': 'PBP F',
     }
 
     return render(request, "main.html", context)
